1109db8e0197c22d8f33173a0d80dd5dcb7f8d14
Michelle Cheng <cheng.michelley@gmail.com>
2026-05-17 20:41:12 -0400

    Initial commit

---
+++ server.js
@@ -0,0 +1,26 @@
+const dotenv = require('dotenv');
+dotenv.config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const logger = require('morgan');
+const testController = require("./controllers/test-jwt")
+const authController = require('./controllers/auth')
+const userController = require('./controllers/user')
+const verifyJwt = require('./middlewares/verify-jwt')
+
+require('./db/connection')
+
+app.use(cors());
+app.use(express.json());
+app.use(logger('dev'));
+
+// Routes
+app.use('/auth', authController)
+
+app.use(verifyJwt)  // Authorization check
+app.use('/users', userController)
+
+app.listen(3000, () => {
+  console.log('The express app is ready!');
+});
